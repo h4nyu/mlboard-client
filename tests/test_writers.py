1c53b581bbab6f187f9a8065030c73e6eaa58861
h4nyu <yao.ntno@gmail.com>
2019-11-13 08:04:14 +0000

    init

---
+++ tests/test_writers.py
@@ -0,0 +1,29 @@
+from mlboard_client.writers import Writer
+from random import random
+import pytest
+
+
+@pytest.fixture
+def writer() -> Writer:
+    return Writer(
+        'http://api:5000/',
+        'test0',
+        {
+            'p0': 12,
+            'p1': {
+                'p3': 'fasdfa'
+            },
+        }
+    )
+
+
+def test_add_scalar(writer: Writer) -> None:
+    writer.add_scaler('aaa/test', 1)
+
+
+def test_add_scalars(writer: Writer) -> None:
+    writer.add_scalers({
+        'aaa': random(),
+        'bbb': random(),
+        'ccc': random(),
+    })
